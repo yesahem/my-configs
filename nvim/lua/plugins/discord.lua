b68a6f58e62570c13ac7256f609ac8f6da527515
yesahem <ahemraj82@gmail.com>
2025-03-23 06:21:48 +0530

    updated  config

--- nvim/lua/plugins/discord.lua
+++ nvim/lua/plugins/discord.lua
@@ -3,6 +3,24 @@ return {
     "vyfor/cord.nvim",
     build = "sh -c './build || .\\build'", -- I use fish
     event = "VeryLazy",
-    opts = {}, -- calls require('cord').setup()
+    opts = {
+      editor = {
+        client = "neovim",
+        tooltip = "Shishu's Workspace",
+      },
+
+      text = {
+        workspace = [[
+Pain is Temporary,
+Pride is Permanenet
+
+				]],
+      },
+      idle = {
+        enable = true,
+        text = "in DND",
+        tooltip = "Having a quality time",
+      },
+    }, -- calls require('cord').setup()
   },
 }
