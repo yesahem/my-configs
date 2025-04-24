7a9479b14f07aaa4294981100d2c87c475d73bd2
yesahem <ahemraj82@gmail.com>
2025-04-24 13:23:23 +0530

    updated nvim configs

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -3,7 +3,7 @@ return {
     "lukas-reineke/virt-column.nvim",
     opts = {
       char = { "┆" },
-      virtcolumn = "80",
+      virtcolumn = "150",
       highlight = { "NonText" },
     },
   },
